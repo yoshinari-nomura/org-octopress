36757f6ba58fbbde0f73bb0531f1be1e73d13956
Yoshinari Nomura <nom@quickhack.net>
2013-03-19 15:15:16 +0900

    first commit

---
+++ org-octopress.el
@@ -0,0 +1,250 @@
+;;; org-octopress.el -- Compose octopress articles using org-mode.
+
+;; Author:  Yoshinari Nomura <nom@quickhack.net>
+;; Keywords: org, jekyll, octopress, blog
+
+;;; Commentay:
+
+;; Basic settings:
+;;
+;; (setq org-octopress-directory-top       "~/octopress/source")
+;; (setq org-octopress-directory-posts     "~/octopress/source/_posts")
+;; (setq org-octopress-directory-org-top   "~/octopress/source")
+;; (setq org-octopress-directory-org-posts "~/octopress/source/blog")
+;; (setq org-octopress-setup-file          "~/lib/org-sty/setupfile.org")
+;;
+;; M-x org-octopress
+;;
+;; Note:
+;;  In octopress/_config.yml, you must set the permelink attribute:
+;;    permalink: /blog/:year-:month-:day-:title.html
+;;
+
+;;; Code:
+
+(require 'org)
+(require 'orglue-publish)
+(require 'ox-jekyll)
+(require 'ctable)
+
+;;; Publishing
+
+;; Assumed directory tree:
+;;
+;; + octopress
+;;   + source
+;;     + blog   <- (1) You compose YYYY-MM-DD-title.org
+;;     + _posts <- (2) ox-jekyll.el exports to YYYY-MM-DD-title.html (w/ YAML)
+;;   + public
+;;     + blog   <- (3) Jekyll exports to YYYY-MM-DD-title.html (w/o YAML).
+;;
+
+(defvar org-octopress-directory-top       "~/octopress/source")
+(defvar org-octopress-directory-posts     "~/octopress/source/_posts")
+(defvar org-octopress-directory-org-top   "~/octopress/source")
+(defvar org-octopress-directory-org-posts "~/octopress/source/blog")
+(defvar org-octopress-setup-file          "~/sys/lib/org-sty/octopress.org")
+
+(add-hook 'orglue-before-export-dispatch-hook 'org-octopress-setup-publish-project)
+
+(defun org-octopress-setup-publish-project ()
+  (let* ((oct-top   org-octopress-directory-top)
+         (org-top   org-octopress-directory-org-top)
+         (oct-posts org-octopress-directory-posts)
+         (org-posts org-octopress-directory-org-posts))
+    (orglue-update-publish-project-alist
+     'org-publish-project-alist
+     `(
+       ("octopress" :components ("octopress-posts" "octopress-org"))
+       ("octopress-posts"
+        :base-directory ,org-posts
+        :publishing-directory ,oct-posts
+        :base-extension "org"
+        :recursive nil
+        :exclude "/[^0-9][^/]+\\.org$" ;; XXXX
+        :publishing-function org-jekyll-publish-to-html
+        )
+       ("octopress-org"
+        :base-directory ,org-top
+        :publishing-directory ,oct-top
+        :base-extension "org"
+        :exclude "[0-9]\\{4\\}-[0-9][0-9]-[0-9][0-9]-.*\\.org$"
+        :recursive t
+        :publishing-function org-jekyll-publish-to-html
+        )
+       ("octopress-static"
+        :base-directory ,org-top
+        :publishing-directory ,oct-top
+        :base-extension ".*"
+        :exclude "\\.org$"
+        :recursive t
+        :publishing-function org-publish-attachment
+        )
+       ))))
+
+;;; Summary Mode
+
+;; keymap
+(defvar org-octopress-summary-mode-map  nil
+  "Keymap for `org-octopress-summary-mode'.")
+
+(defvar org-octopress-summary-mode-hook nil)
+
+(defun org-octopress--merge-keymap (keymap1 keymap2)
+  (append keymap1
+          (delq nil
+                (mapcar
+                 (lambda (x)
+                   (if (or (not (consp x))
+                           (assoc (car x) keymap1))
+                       nil x))
+                 keymap2))))
+
+(unless org-octopress-summary-mode-map
+  (setq org-octopress-summary-mode-map (make-sparse-keymap))
+  (define-key org-octopress-summary-mode-map "w" 'org-octopress-new-post)
+  (define-key org-octopress-summary-mode-map "d" 'org-octopress-delete-post)
+  (setq org-octopress-summary-mode-map
+        (org-octopress--merge-keymap org-octopress-summary-mode-map ctbl:table-mode-map)))
+
+;; new post
+(defun org-octopress-new-post (&optional title date)
+  "New post."
+  (interactive "sPermalink Text: ")
+  (let ((date (or date (org-read-date))))
+    (find-file (expand-file-name
+                (org-octopress--new-post-file-name title date)
+                org-octopress-directory-org-posts))
+    (save-excursion
+      (org-jekyll-insert-export-options-template title date org-octopress-setup-file nil "true"))
+    (search-forward "TITLE: " nil t)))
+
+;; delete post
+(defun org-octopress-delete-post ()
+  "Delete existing post."
+  (interactive)
+  (message "Not yet implemented."))
+
+;; summary 
+(defun org-octopress-summary-mode ()
+  "Major mode for listing and controlling org-mode based blog articles.
+
+\\{org-octopress-summary-mode-map}"
+  (kill-all-local-variables)
+  (setq truncate-lines t)
+  (use-local-map org-octopress-summary-mode-map)
+  (setq major-mode 'org-octopress-summary-mode
+        mode-name  "Org-Octopress")
+  (setq buffer-undo-list t
+        buffer-read-only t)
+  (run-hooks 'org-octopress-summary-mode-hook))
+
+(defun org-octopress--summary-header (&optional title)
+  (concat
+   (format "%s\n" (or title "Octopress"))
+   (mapconcat
+    'identity
+    (org-octopress--summary-command-help
+     (remove-duplicates
+      (mapcar 'cdr (cdr org-octopress-summary-mode-map)))
+     org-octopress-summary-mode-map)
+    "\n")
+   "\n\n\n"))
+
+(defun org-octopress--summary-command-help (symbols &optional keymap)
+  (let (symbol keysym keystr docstr summary-list)
+    (while (setq symbol (car symbols))
+      (setq keysym (where-is-internal symbol (or keymap (current-local-map)) nil)
+            keystr (if keysym (mapconcat 'key-description keysym ",") "No keybind")
+            docstr (documentation symbol))
+      (if docstr
+          (setq summary-list (cons (format "%10s ... %s (%s)"
+                                           keystr
+                                           (car (split-string docstr "\n"))
+                                           symbol)
+                                   summary-list)))
+      (setq symbols (cdr symbols)))
+    summary-list))
+
+(defun org-octopress--summary-table (contents keymap)
+  (let ((param (copy-ctbl:param ctbl:default-rendering-param)))
+    (setf (ctbl:param-fixed-header param) t)
+    (ctbl:create-table-component-region
+     :param param
+     :width  nil
+     :height nil
+     :keymap keymap
+     :model
+     (make-ctbl:model
+      :data contents
+      :sort-state '(-1 2)
+      :column-model
+      (list (make-ctbl:cmodel
+             :title "Date"
+             :sorter 'ctbl:sort-string-lessp
+             :min-width 10
+             :align 'left)
+            (make-ctbl:cmodel
+             :title "Category"
+             :align 'left
+             :sorter 'ctbl:sort-string-lessp)
+            (make-ctbl:cmodel
+             :title "Title"
+             :align 'left
+             :min-width 40
+             :max-width 140)
+            (make-ctbl:cmodel
+             :title "Published"
+             :align 'left
+             :min-width 4
+             :max-width 5)
+            )))))
+
+(defun org-octopress--scan-post ()
+  (mapcar
+   (lambda (filename)
+     (org-jekyll-property
+      '(:date
+        :jekyll-categories
+        :title
+        :jekyll-published
+        :input-file)
+      filename))
+   (directory-files
+    (expand-file-name
+     org-octopress-directory-org-posts) t "^[0-9].*\\.org$")))
+
+;; startup
+(defun org-octopress (&optional title)
+  "Org-mode and Octopress."
+  (interactive)
+  (let ((buf (get-buffer-create "Octpress"))
+        (cp))
+    (switch-to-buffer buf)
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (insert (org-octopress--summary-header title))
+    (save-excursion
+      (setq cp (org-octopress--summary-table 
+                (org-octopress--scan-post) org-octopress-summary-mode-map)))
+    (ctbl:cp-add-click-hook
+     cp
+     (lambda ()
+       (find-file (nth 4 (ctbl:cp-get-selected-data-row cp)))))
+    (org-octopress-summary-mode)
+    (ctbl:navi-goto-cell
+     (ctbl:find-first-cell (ctbl:component-dest cp)))
+    ))
+
+;;; Helpers
+
+(defun org-octopress--sanitize-title (title)
+  (replace-regexp-in-string "[\t ]+" "-" (downcase title)))
+
+(defun org-octopress--new-post-file-name (title &optional date)
+  (let ((time (if (stringp date) (org-read-date nil t date) date)))
+    (format
+     (format-time-string "%Y-%m-%d-%%s.org" time)
+     (org-octopress--sanitize-title title))))
+
+(provide 'org-octopress)
---
+++ ox-jekyll.el
@@ -0,0 +1,244 @@
+;;; ox-jekyll.el --- Export Jekyll articles using org-mode.
+
+;; Copyright (C) 2013  Yoshinari Nomura
+
+;; Author: Yoshinari Nomura <nom@quickhack.net>
+;; Keywords: org, jekyll
+
+;;; Commentary:
+
+;; This library implements a Jekyll-style html backend for
+;; Org exporter, based on `html' back-end.
+;;
+;; It provides two commands for export, depending on the desired
+;; output: `org-jkl-export-as-html' (temporary buffer) and
+;; `org-jkl-export-to-html' ("html" file with YAML front matter).
+;;
+;; For publishing, `org-jekyll-publish-to-html' is available.
+;; For composing, `org-jekyll-insert-export-options-template' is available.
+
+;;; Code:
+
+;;; Dependencies
+
+(eval-when-compile (require 'cl))
+(require 'ox-html)
+
+;;; User Configurable Variables
+
+(defgroup org-export-jekyll nil
+  "Options for exporting Org mode files to jekyll HTML."
+  :tag "Org Jekyll"
+  :group 'org-export
+  :version "24.2")
+
+(defcustom org-jekyll-layout "post"
+  "Default layout used in Jekyll article."
+  :group 'org-export-jekyll
+  :type 'string)
+
+(defcustom org-jekyll-categories ""
+  "Default space-separated categories in Jekyll article."
+  :group 'org-export-jekyll
+  :type 'string)
+
+(defcustom org-jekyll-published "true"
+  "Default publish status in Jekyll article."
+  :group 'org-export-jekyll
+  :type 'string)
+
+
+;;; Define Back-End
+
+(org-export-define-derived-backend jekyll html
+  :export-block ("HTML" "JEKYLL")
+  :menu-entry
+  (?j "Jekyll: export to HTML with YAML front matter."
+      ((?H "As HTML buffer" org-jekyll-export-as-html)
+       (?h "As HTML file" org-jekyll-export-to-html)))
+  :translate-alist
+  ((template . org-jekyll-template) ;; add YAML front matter.
+   (inner-template . org-jekyll-inner-template)) ;; force body-only
+  :options-alist
+  ((:jekyll-layout "JEKYLL_LAYOUT" nil org-jekyll-layout)
+   (:jekyll-categories "JEKYLL_CATEGORIES" nil org-jekyll-categories)
+   (:jekyll-published "JEKYLL_PUBLISHED" nil org-jekyll-published)))
+
+
+;;; Internal Filters
+
+;;; Template
+
+(defun org-jekyll-template (contents info)
+  "Return complete document string after HTML conversion.
+CONTENTS is the transcoded contents string. INFO is a plist
+holding export options."
+  (concat
+   (org-jekyll--yaml-front-matter info)
+   contents))
+
+(defun org-jekyll-inner-template (contents info)
+  "Return body of document string after HTML conversion.
+CONTENTS is the transcoded contents string.  INFO is a plist
+holding export options."
+  (concat
+   ;; Table of contents.
+   (let ((depth (plist-get info :with-toc)))
+     (when depth (org-html-toc depth info)))
+   ;; PREVIEW mark on the top of article.
+   (unless (equal "true" (plist-get info :jekyll-published))
+     "<span style=\"background: red;\">PREVIEW</span>")
+   ;; Document contents.
+   contents
+   ;; Footnotes section.
+   (org-html-footnote-section info)
+   ;; Bibliography.
+   (org-html-bibliography)))
+
+;;; YAML Front Matter
+
+(defun org-jekyll--get-option (info property-name &optional default)
+  (let ((property (org-export-data (plist-get info property-name) info)))
+    (format "%s" (or property default ""))))
+
+(defun org-jekyll--yaml-front-matter (info)
+  (let ((title
+         (org-jekyll--get-option info :title))
+        (date
+         (org-jekyll--get-option info :date))
+        (layout
+         (org-jekyll--get-option info :jekyll-layout org-jekyll-layout))
+        (categories
+         (org-jekyll--get-option info :jekyll-categories org-jekyll-categories))
+        (published
+         (org-jekyll--get-option info :jekyll-published org-jekyll-published))
+        (comments
+         (org-jekyll--get-option info :jekyll-comments)))
+    (unless (equal published "true")
+      (setq title (concat "[PREVIEW] " title)))
+    (concat
+     "---"
+     "\ntitle: \""    title
+     "\"\ndate: "     date
+     "\nlayout: "     layout
+     "\ncategories: " categories
+     "\npublished: "  published
+     "\ncomments: "   comments
+     "\n---\n")))
+
+;;; Filename and Date Helper
+
+(defun org-jekyll-date-from-filename (&optional filename)
+  (let ((fn (file-name-nondirectory (or filename (buffer-file-name)))))
+    (if (string-match "^[0-9]+-[0-9]+-[0-9]+" fn)
+        (match-string 0 fn)
+      nil)))
+
+(defun org-jekyll-property-list (&optional filename)
+  (let ((backend 'jekyll) plist)
+    (if filename
+        (with-temp-buffer
+          (insert-file filename)
+          (org-mode)
+          (setq plist (org-export-get-environment backend))
+          (setq plist (plist-put plist :input-file filename)))
+      (setq plist (org-export-backend-options backend))
+      plist)))
+
+(defun org-jekyll-property (keys &optional filename)
+  (let ((plist (org-jekyll-property-list filename)))
+    (mapcar (lambda (key) (org-export-data-with-backend (plist-get plist key) 'jekyll plist))
+            keys)))
+
+(defun org-jekyll-date-from-property (&optional filename)
+  (let ((plist (org-jekyll-property filename)))
+    (org-read-date
+     nil nil
+     (org-export-data-with-backend (plist-get plist :date) backend plist))))
+
+(defun org-jekyll-create-filename ()
+  (let ((date (org-jekyll-date-from-property))
+        (file (file-name-nondirectory (buffer-file-name)))
+        (dir  (file-name-directory (buffer-file-name))))
+    (expand-file-name
+     (replace-regexp-in-string "^[0-9]+-[0-9]+-[0-9]+" date file)
+     dir)))
+
+;;; End-User functions
+
+;;;###autoload
+(defun org-jekyll-export-as-html
+  (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer to a HTML buffer adding some YAML front matter."
+  (interactive)
+  (if async
+      (org-export-async-start
+          (lambda (output)
+            (with-current-buffer (get-buffer-create "*Org Jekyll HTML Export*")
+              (erase-buffer)
+              (insert output)
+              (goto-char (point-min))
+              (funcall org-html-display-buffer-mode)
+              (org-export-add-to-stack (current-buffer) 'jekyll)))
+        `(org-export-as 'jekyll ,subtreep ,visible-only ,body-only ',ext-plist))
+    (let ((outbuf (org-export-to-buffer
+                   'jekyll "*Org Jekyll HTML Export*"
+                   subtreep visible-only body-only ext-plist)))
+      ;; Set major mode.
+      (with-current-buffer outbuf (funcall org-html-display-buffer-mode))
+      (when org-export-show-temporary-export-buffer
+        (switch-to-buffer-other-window outbuf)))))
+
+;;;###autoload
+(defun org-jekyll-export-to-html
+  (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer to a HTML file adding some YAML front matter."
+  (interactive)
+  (let* ((extension (concat "." org-html-extension))
+         (file (org-export-output-file-name extension subtreep))
+         (org-export-coding-system org-html-coding-system))
+    (if async
+        (org-export-async-start
+            (lambda (f) (org-export-add-to-stack f 'jekyll))
+          (let ((org-export-coding-system org-html-coding-system))
+            `(expand-file-name
+              (org-export-to-file
+               'jekyll ,file ,subtreep ,visible-only ,body-only ',ext-plist))))
+      (let ((org-export-coding-system org-html-coding-system))
+        (org-export-to-file
+         'jekyll file subtreep visible-only body-only ext-plist)))))
+
+;;;###autoload
+(defun org-jekyll-publish-to-html (plist filename pub-dir)
+  "Publish an org file to HTML with YAML front matter.
+
+FILENAME is the filename of the Org file to be published.  PLIST
+is the property list for the given project.  PUB-DIR is the
+publishing directory.
+
+Return output file name."
+  (org-publish-org-to 'jekyll filename ".html" plist pub-dir))
+
+;;;###autoload
+(defun org-jekyll-insert-export-options-template
+  (&optional title date setupfile categories published layout)
+  "Insert a settings template for Jekyll exporter."
+  (interactive)
+  (let ((layout     (or layout org-jekyll-layout))
+        (published  (or published org-jekyll-published))
+        (categories (or categories org-jekyll-categories)))
+    (save-excursion
+      (insert (format (concat
+                       "#+TITLE: "             title
+                       "\n#+DATE: "              date
+                       "\n#+SETUPFILE: "         setupfile
+                       "\n#+JEKYLL_LAYOUT: "     layout
+                       "\n#+JEKYLL_CATEGORIES: " categories
+                       "\n#+JEKYLL_PUBLISHED: "  published
+                       "\n\n* \n\n{{{more}}}"))))))
+
+;;; provide
+
+(provide 'ox-jekyll)
+
+;;; ox-jekyll.el ends here
